6a65e91da071b06225281a2c6e00c5a85695f1be
jackmanjim <opjacks@gmail.com>
2021-03-26 16:22:40 +0800

    first

---
+++ js/test.js
@@ -0,0 +1,36 @@
+
+
+
+// Swiper function start (meal menu)
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+    });
+
+// Swiper function end 
+
+
+// Swiper function start (other)
+    // var swiper = new Swiper('.swiper-container', {
+    //   spaceBetween: 30,
+    //   centeredSlides: true,
+    //   autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    //   },
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //   },
+
+    // });
+// Swiper function end 
